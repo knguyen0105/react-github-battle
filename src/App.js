d9386a36399a5f449267de3f8188712c3979cae7
Khanh Nguyen Hong (BTS-HO) <khanh.nguyenhong@north.vib.corp>
2017-04-25 14:19:23 +0700

    add popular component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Popular extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    }
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
+  render() {
+    var languages = ['All','Javascript','Ruby','Java','CSS','Python'];
+
+    return (
+      
+      <ul className='languages'>
+        {languages.map((lang) => {
+          return (
+            <li
+              style={lang===this.state.selectedLanguage ? {color:'#d0021b'}:null} 
+              key={lang} 
+              onClick={this.updateLanguage.bind(null,lang)}>
+               {lang}
+            </li>)
+        })}
+      </ul>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='container'>
+        <Popular />
       </div>
     );
   }
